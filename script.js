303036f37600376a5120eb3d2ef4470a412eb9e6
Karla Vujevic <karla_vu@icloud.com>
2023-09-03 16:11:20 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const rootNode = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootNode)
+
+function App(){
+    //return React.createElement("h1", null, "Hello React")
+    return React.createElement("div", null,
+        React.createElement("h1", null, "Hello React"),
+        React.createElement("p", null, "Novi modul React"),
+        React.createElement("ul", null,
+            React.createElement("li", null, "Subtitle 1"),
+            React.createElement("li", null, "Subtitle 2"),
+            React.createElement("li", null, "Subtitle 3"),
+            )
+    )
+}
+
+const virtualDOM = App()
+//const virtualDOM = React.createElement("h1", null, "Hello React")
+console.log(virtualDOM)
+
+root.render(React.createElement(App))
+
+/*const VD = {
+    type: "h1",
+    children: "Hello React"
+}*/
